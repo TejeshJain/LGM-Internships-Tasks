4715f1b60855ed956bd520041f2ddb26d5848bac
Tejesh Jain <tejeshjain2521@gmail.com>
2023-06-27 12:31:21 +0530

    update 3.0

--- LGMVIP-Web-Task-2/src/App.js
+++ LGMVIP-Web-Task-2/src/App.js
@@ -1,19 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import UserCard from './UserCard';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import UserCard from "./UserCard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
-  const[Flag,setFlag]= useState(false)
+  const [Flag, setFlag] = useState(false);
   const getUsers = async () => {
     setLoading(true);
     try {
-      const response = await axios.get('https://reqres.in/api/users?page=1');
+      const response = await axios.get("https://reqres.in/api/users?page=1");
       setFlag(!Flag);
       setUsers(response.data.data);
-     
+
       setLoading(false);
     } catch (error) {
       console.log(error);
@@ -24,26 +28,60 @@ function App() {
     getUsers();
   }, []);
 
-
-  
   return (
     <div>
-      <nav className="navbar">
-        {/* <span className="brand-name" style={{marginRight:"12px", fontWeight:"bold"}}>Tejesh Jain</span> */}
-        <button className="get-users" onClick={getUsers} disabled={loading} style={{padding:"12px", borderRadius:"4px", marginTop:"50px"}}>
-          Get Users
-        </button>
-      </nav>
-      {Flag===true ? (<div className="user-card" style={{display:"flex",flexWrap:"wrap", marginTop:"100px"}}>
-        {loading ? (
-          <div className="loader"><h1>Loading...</h1></div>
-        ) : (
-           users.map(user => <UserCard key={user.id} user={user} />)
-        )}
-      </div>):""}
+      <Navbar expand="lg" className="bg-body-tertiary ">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav
+              className="me-auto"
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                minWidth:"100%"
+              }}
+            >
+              <Navbar.Brand href="#home">Lets Grow More</Navbar.Brand>
+              <button
+                className="get-users"
+                onClick={getUsers}
+                disabled={loading}
+                style={{
+                  padding: "12px",
+                  borderRadius: "4px",
 
+                  // marginTop: "50px",
+                  // alignItems:"center",
+                }}
+              >
+                Get Users
+              </button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* <span className="brand-name" style={{marginRight:"12px", fontWeight:"bold"}}>Tejesh Jain</span> */}
+
+      {Flag === true ? (
+        <div
+          className="user-card"
+          style={{ display: "flex", flexWrap: "wrap", marginTop: "100px" }}
+        >
+          {loading ? (
+            <div className="loader">
+              <h1>Loading...</h1>
+            </div>
+          ) : (
+            users.map((user) => <UserCard key={user.id} user={user} />)
+          )}
+        </div>
+      ) : (
+        ""
+      )}
     </div>
-    
   );
 }
 
